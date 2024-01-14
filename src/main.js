3343fed7c5a90f09ccb00ed44abec9de1b155957
anyalenzy <anya.dudnik.91@gmail.com>
2024-01-15 00:15:39 +0200

    done js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
-import errorImg from '/img/error.svg';
+import errorIcon from '/img/error.svg';
 
 const url = 'https://pixabay.com/api/';
 const searchParams = {
@@ -17,7 +17,86 @@ const simpleGallery = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 
-const form = document.querySelector('.gallery-form');
+const form = document.querySelector('.form');
 const searchInput = document.querySelector('.search-input');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!searchInput.value.trim()) {
+    showErrormsg('Please, fill out the search field');
+    return;
+  }
+  fetchPhotos()
+    .then(photos => createGallery(photos))
+    .catch(error => showErrormsg(error.toString()));
+});
+
+function fetchPhotos() {
+  gallery.innerHTML = '';
+  loader.style.display = 'inline-block';
+  searchParams.q = searchInput.value.trim();
+  const searchParamsString = new URLSearchParams(searchParams).toString();
+  return fetch(`${url}?${searchParamsString}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+function createGallery(photos) {
+  if (photos.hits.length === 0) {
+    showErrormsg(
+      'Sorry, there are no images matching your search query. Please try again!'
+    );
+    loader.style.display = 'none';
+    return;
+  }
+  const markup = photos.hits
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+        <li class="gallery-item">
+          <a href="${largeImageURL}">
+            <img class="gallery-image" src="${webformatURL}" alt="${tags}">
+            <div class="img-desc">
+              <span><b>Likes:</b> <br/>${likes}</span>
+              <span><b>Views:</b> <br/>${views}</span>
+              <span><b>Comments:</b> <br/>${comments}</span>
+              <span><b>Downloads:</b> <br/>${downloads}</span>
+            </div>
+          </a>
+        </li>
+                  `;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('afterbegin', markup);
+  loader.style.display = 'none';
+  searchParams.q = '';
+  simpleGallery.refresh();
+  form.reset();
+}
+
+function showErrormsg(msg) {
+  iziToast.show({
+    position: 'topRight',
+    iconUrl: errorIcon,
+    messageColor: '#FAFAFB',
+    messageSize: '16px',
+    backgroundColor: '#EF4040',
+    close: false,
+    // closeOnClick: true,
+    closeOnEscape: true,
+    message: msg,
+  });
+}
